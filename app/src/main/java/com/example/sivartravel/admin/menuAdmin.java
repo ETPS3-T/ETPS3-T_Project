3ff51d3fd871187e4b4a3d4490607b8cf9d605ab
dennyschico100 <dennyschico2@gmail.com>
2021-03-06 20:25:54 -0600

    xml de info de usuario

---
+++ app/src/main/java/com/example/sivartravel/admin/menuAdmin.java
@@ -0,0 +1,17 @@
+package com.example.sivartravel.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sivartravel.R;
+
+public class menuAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_menu_admin);
+    }
+}
