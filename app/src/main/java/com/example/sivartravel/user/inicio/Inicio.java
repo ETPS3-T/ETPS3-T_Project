3ccf0ed183bb27aef16e18846a222de7edb4f035
Jose Daniel Pascasio <joshepascasio.12@gmail.com>
2021-03-12 01:44:54 -0600

    Activity "Lugares" finalizado y funcionando. Queda quemar 8 departamentos con mas informacion.

--- app/src/main/java/com/example/sivartravel/user/inicio/Inicio.java
+++ app/src/main/java/com/example/sivartravel/user/inicio/Inicio.java
@@ -37,11 +37,7 @@ public class Inicio extends Fragment  implements InterfaceClickListener {
        View root = inflater.inflate(R.layout.user_inicio, container, false);
 
         rvLugares = (RecyclerView)root.findViewById(R.id.rvLugares);
-
-        if(getLugares().isEmpty()){
-            getLugares();
-        }
-
+        getLugares();
         rvLugares.setLayoutManager(new LinearLayoutManager(getContext()));
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(all, this);
         rvLugares.setAdapter(adapter);
@@ -50,15 +46,17 @@ public class Inicio extends Fragment  implements InterfaceClickListener {
     }
 
     private ArrayList<LugaresEntity> getLugares(){
+
         all= new ArrayList<>();
-        all.add(new LugaresEntity(R.drawable.nahuizalco, "Nahuizalco","Sonsonate","Pueblo vivo","Nahuizalco es un poblado de origen prehispánico. Originalmente fue habitado por Náhuas de Kuskatan, y en la época colonial la región perteneció a la Provincia de los Izalcos.\n De acuerdo a una crónica de 1586, se estimaba que los residentes no llegaban a 200 personas.","9:00 am - 17:00 pm", "",""));
-        all.add(new LugaresEntity(R.drawable.suchitoto, "Suchitoto","Suchitoto", "Pueblo vivo","Suchitoto (en idioma Náhuat \"Shuchitutut\" (Flor-Pájaro) es un municipio del departamento de Cuscatlán, El Salvador.\n Su territorio ha sido habitado desde la época precolombina, y también fue el sitio donde se fundó la villa de San Salvador en 1528, que tuvo una breve existencia.","6:00 am - 20:00 pm", "",""));
-        all.add(new LugaresEntity(R.drawable.apaneca, "Apaneca","Ahuachapan", "Pueblo vivo","Ahuachapán fue fundado en el siglo V por los mayas de la tribu pokomames, y sometida en el siglo XV por belicosos Náhuas de Kuskatan de los izalcos.\n Gradualmente la región fue invadida por los españoles.","24/7", "",""));
+        all.add(new LugaresEntity(R.drawable.main, "Nahuizalco","Sonsonate","Pueblo vivo","Nahuizalco es un poblado de origen prehispánico. Originalmente fue habitado por Náhuas de Kuskatan, y en la época colonial la región perteneció a la Provincia de los Izalcos.\n De acuerdo a una crónica de 1586, se estimaba que los residentes no llegaban a 200 personas.","9:00 am - 17:00 pm", "",""));
+        all.add(new LugaresEntity(R.drawable.main, "Suchitoto","Suchitoto", "Pueblo vivo","Suchitoto (en idioma Náhuat \"Shuchitutut\" (Flor-Pájaro) es un municipio del departamento de Cuscatlán, El Salvador.\n Su territorio ha sido habitado desde la época precolombina, y también fue el sitio donde se fundó la villa de San Salvador en 1528, que tuvo una breve existencia.","6:00 am - 20:00 pm", "",""));
+        all.add(new LugaresEntity(R.drawable.main, "Apaneca","Ahuachapan", "Pueblo vivo","Ahuachapán fue fundado en el siglo V por los mayas de la tribu pokomames, y sometida en el siglo XV por belicosos Náhuas de Kuskatan de los izalcos.\n Gradualmente la región fue invadida por los españoles.","24/7", "",""));
         return all;
     }
 
     @Override
     public void OnItemClick(int position) {
+        
         if(all != null && all.size() > 0)
         {
             info_lugar_fragmento fr=new info_lugar_fragmento();
--- app/src/main/java/com/example/sivartravel/user/lugares/Lugares.java
+++ app/src/main/java/com/example/sivartravel/user/lugares/Lugares.java
@@ -14,8 +14,15 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sivartravel.R;
+import com.example.sivartravel.user.adaptador.InterfaceClickListener;
+import com.example.sivartravel.user.adaptador.RecyclerViewAdapter;
+import com.example.sivartravel.user.adaptador.RecyclerViewAdapterMini;
+import com.example.sivartravel.user.entitys.LugaresEntity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -26,24 +33,37 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Lugares extends Fragment implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class Lugares extends Fragment implements OnMapReadyCallback, InterfaceClickListener {
 
     private GoogleMap mimapa;
     private MapView mMapView;
     private Spinner spDepartures;
     private View root;
 
+    private RecyclerView Rec;
+    private RecyclerView.LayoutManager adapter;
+    private ArrayList<LugaresEntity> x = new ArrayList<>();;
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.user_lugares, container, false);
         spDepartures = root.findViewById(R.id.spDepa);
+        Rec = (RecyclerView)root.findViewById(R.id.Rec);
+
 
         spDepartures.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             public void onItemSelected(AdapterView<?> spn,View v,int position ,long id) {
+
                 String dep = (String) spn.getItemAtPosition(position);
                 switch(dep)
                 {
                     case "Chalatenango":
+
+                        ada(Chalate());
                         mimapa.clear();
                         mimapa.addMarker(new MarkerOptions().position(new LatLng(14.0227471, -88.9384462)).title("Chalatenango").snippet("NICE"));
                         CameraPosition CHA = CameraPosition.builder().target(new LatLng(14.0227471, -88.9384462)).zoom(10).bearing(0).tilt(35).build();
@@ -51,6 +71,8 @@ public class Lugares extends Fragment implements OnMapReadyCallback {
 
                         break;
                     case "San Salvador":
+
+                        ada(SanSalvador());
                         mimapa.clear();
                         mimapa.addMarker(new MarkerOptions().position(new LatLng(13.6915591, -89.2502715)).title("San Salvador").snippet("NICE"));
                         CameraPosition SS = CameraPosition.builder().target(new LatLng(13.6915591, -89.2502715)).zoom(10).bearing(0).tilt(35).build();
@@ -58,6 +80,7 @@ public class Lugares extends Fragment implements OnMapReadyCallback {
 
                         break;
                     case "Cabañas":
+                        ada(Cabanas());
                         mimapa.clear();
                         mimapa.addMarker(new MarkerOptions().position(new LatLng(13.8796805, -88.8809559)).title("Cabañas").snippet("NICE"));
                         CameraPosition CA = CameraPosition.builder().target(new LatLng(13.8796805, -88.8809559)).zoom(10).bearing(0).tilt(35).build();
@@ -95,6 +118,7 @@ public class Lugares extends Fragment implements OnMapReadyCallback {
                         break;
 
                     case "La Libertad":
+                        ada(LaLibertad());
                         mimapa.clear();
                         mimapa.addMarker(new MarkerOptions().position(new LatLng(13.8523851, -89.4360627)).title("La Libertad").snippet("NICE"));
                         CameraPosition LALI = CameraPosition.builder().target(new LatLng(13.8523851, -89.4360627)).zoom(10).bearing(0).tilt(35).build();
@@ -141,6 +165,7 @@ public class Lugares extends Fragment implements OnMapReadyCallback {
 
                         break;
                     case "Santa Ana":
+                        ada(SantaAna());
                         mimapa.clear();
                         mimapa.addMarker(new MarkerOptions().position(new LatLng(13.9838741, -89.5978411)).title("Santa Ana").snippet("NICE"));
                         CameraPosition SNT = CameraPosition.builder().target(new LatLng(13.9838741, -89.5978411)).zoom(10).bearing(0).tilt(35).build();
@@ -154,14 +179,75 @@ public class Lugares extends Fragment implements OnMapReadyCallback {
             }
         });
 
+        RecyclerViewAdapterMini adapter = new RecyclerViewAdapterMini(x, this);
+        Rec.setLayoutManager(new GridLayoutManager(getContext(),2));
+        Rec.setAdapter(adapter);
+
         return root;
     }
+    private ArrayList<LugaresEntity> Chalate(){
+
+        x.clear();
+        x.add(new LugaresEntity(R.drawable.main, "Rio Sumpul","Chalatenango","CENTRO TURISTICO","","9:00 am - 17:00 pm", "14.0672518","-88.8947239"));
+        x.add(new LugaresEntity(R.drawable.main, "Aguas Frias","Chalatenango", "CENTRO TURISTICO","","6:00 am - 20:00 pm", "14.0499338","-88.937296"));
+        x.add(new LugaresEntity(R.drawable.main, "Cerro El Pital","Chalatenango", "CENTRO TURISTICO","","24/7", "14.3805098","-89.1466227"));
+        x.add(new LugaresEntity(R.drawable.main, "Entre Pinos","Chalatenango", "HOTEL","","24/7", "14.3312982","-89.1804379"));
+
+        return x;
+    }
+    private ArrayList<LugaresEntity> SanSalvador(){
+        x.clear();
+        x.add(new LugaresEntity(R.drawable.main, "El Boquerón","San Salvador","PARQUE NACIONAL","","9:00 am - 17:00 pm", "13.7044008","-89.2778442"));
+        x.add(new LugaresEntity(R.drawable.main, "Parque Bicentenario","San Salvador", "PARQUE NACIONAL","","6:00 am - 20:00 pm", "13.6939506","-89.2510802"));
+        x.add(new LugaresEntity(R.drawable.main, " Lago Ilopango","San Salvador", "CENTRO TURISTICO","","24/7", "13.6629697","-89.0107873"));
+        x.add(new LugaresEntity(R.drawable.main, "Joya de Cerén","San Salvador", "SITIO ARQUEOLOGICO","","24/7", "13.827925","-89.3584326"));
+
+        return x;
+    }
+    private ArrayList<LugaresEntity> SantaAna(){
+        x.clear();
+        x.add(new LugaresEntity(R.drawable.main, "Volcan Santa Ana","Santa Ana","PARQUE NACIONAL","","9:00 am - 17:00 pm", "13.8472748","-89.597247"));
+        x.add(new LugaresEntity(R.drawable.main, "Parque Cerro Verde","Santa Ana", "PARQUE NACIONAL","","6:00 am - 20:00 pm", "13.8472748","-89.597247"));
+        x.add(new LugaresEntity(R.drawable.main, " Lago Coatepeque","Santa Ana", "CENTRO TURISTICO","","24/7", "13.867523","-89.5578335"));
+        x.add(new LugaresEntity(R.drawable.main, "Volcan Ilamatepec","Santa Ana", "PARQUE NACIONAL","","24/7", "13.8502055","-89.653759"));
+
+        return x;
+    }
+    private ArrayList<LugaresEntity> LaLibertad(){
+
+        x.clear();
+        x.add(new LugaresEntity(R.drawable.main, "La Curva de Don Gere","La Libertad","RESTAURANT/BEACH","","9:00 am - 17:00 pm", "13.5016112","-89.4105319"));
+        x.add(new LugaresEntity(R.drawable.main, "El Malecón","La Libertad", "PLAZA MUNICIPAL","","6:00 am - 20:00 pm", "13.4840151","-89.3151255"));
+        x.add(new LugaresEntity(R.drawable.main, "Parque Walter T. Deininger","La Libertad", "PARQUE NACIONAL","","24/7", "13.5036888","-89.2787378"));
+        x.add(new LugaresEntity(R.drawable.main, "San Andrés","La Libertad", "SITIO ARQUEOLOGICO","","24/7", "13.7834365","-89.3830932"));
+
+        return x;
+    }
+    private ArrayList<LugaresEntity> Cabanas(){
+
+        x.clear();
+        x.add(new LugaresEntity(R.drawable.main, "Bosque De Cinquera","Cabañas","PARQUE ECOLOGICO","","9:00 am - 17:00 pm", "13.8796803","-88.881296"));
+        x.add(new LugaresEntity(R.drawable.main, "El Salto San Antonio","Cabañas", "NATURAL CASCADE","","6:00 am - 20:00 pm", "13.9881533","-88.6753741"));
+        x.add(new LugaresEntity(R.drawable.main, "Heliconias Del Rio","Cabañas", "HOTEL","","24/7", "13.8718591","-88.9285789"));
+        x.add(new LugaresEntity(R.drawable.main, "La Casa de La Hacienda","Cabañas", "PARQUE ACUATICO","","24/7", "13.8214476","-88.8582877"));
+
+        return x;
+    }
+
+    public void ada(ArrayList<LugaresEntity> je){
+
+        RecyclerViewAdapterMini adapter = new RecyclerViewAdapterMini(je, this);
+        Rec.setLayoutManager(new GridLayoutManager(getContext(),2));
+        Rec.setAdapter(adapter);
+    }
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
         super.onViewCreated(view,savedInstanceState);
 
         mMapView = (MapView) root.findViewById(R.id.mapView);
         if(mMapView != null){
+
             mMapView.onCreate(null);
             mMapView.onResume();
             mMapView.getMapAsync(this);
@@ -181,4 +267,13 @@ public class Lugares extends Fragment implements OnMapReadyCallback {
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(ESA));
     }
 
+    @Override
+    public void OnItemClick(int position) {
+
+        mimapa.clear();
+        mimapa.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(x.get(position).getX()), Double.parseDouble(x.get(position).getY()))).title(x.get(position).getDescripcion()).snippet("NICE"));
+        CameraPosition CHA = CameraPosition.builder().target(new LatLng(Double.parseDouble(x.get(position).getX()), Double.parseDouble(x.get(position).getY()))).zoom(15).bearing(0).tilt(35).build();
+        mimapa.moveCamera(CameraUpdateFactory.newCameraPosition(CHA));
+
+    }
 }
