fcbcd53e69f4c9db8baac4801c0680458d710421
Douglas Isaias Valle Ortiz <58859695+IsaiasOrtiz@users.noreply.github.com>
2021-05-03 21:13:55 -0600

    Agregandole .setLenient a GsonBuilder para conversion de JSON complejos.

--- app/src/main/java/com/example/sivartravel/restservice/RetrofitClient.java
+++ app/src/main/java/com/example/sivartravel/restservice/RetrofitClient.java
@@ -4,6 +4,8 @@ import com.example.sivartravel.Constantes;
 import com.example.sivartravel.entidades.Lugares;
 import com.example.sivartravel.entidades.Transporte;
 import com.example.sivartravel.Constantes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -13,12 +15,18 @@ public class RetrofitClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String baseUrl) {
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
+
         return retrofit;
     }
 
